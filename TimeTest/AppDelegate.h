e6263af888c7809ee91874661fb838d96e396503
liaoshe <416862100@qq.com>
2017-02-20 10:33:52 +0800

    封装日历
    上传

---
+++ TimeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimeTest
+//
+//  Created by Jimmy_Lee on 16/7/21.
+//  Copyright © 2016年 Jimmy_Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
